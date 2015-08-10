bab8197da4155edd140629ad525d119ec97df38d
guillaume_barranco <guillaume.barranco@playapp.net>
2015-08-10 11:21:15 +0200

    add rubis and other things

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,27 @@
 module.exports = function(grunt) {
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     sass: {
       dist: {
         options:{
           style:'compressed'
+          // sourceMap: true
         },
         files: {
           'stylesheets/index.css' : 'sass/index.scss'
         }
       }
     },
+
+    libsass: {
+        myTarget: {
+            src: 'sass/index.scss',
+            dest: 'stylesheets/index.css',
+        }
+    },
+
     autoprefixer:{
       dist:{
         files:{
@@ -20,11 +31,14 @@ module.exports = function(grunt) {
     },
     watch: {
       css: {
+        options: { livereload: true },
         files: 'sass/*.scss',
-        tasks: ['sass', 'autoprefixer']
+        tasks: ['sass']
       }
     }
   });
+  grunt.loadNpmTasks('grunt-libsass');
+  // grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-autoprefixer');
